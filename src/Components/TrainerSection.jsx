a3e07c42032f646834bdc6ea5217f2ae5e01a10c
abunaim34 <farhanadnanfarabi@gmail.com>
2024-09-20 18:57:22 +0600

    meat section is start

--- src/Components/TrainerSection.jsx
+++ src/Components/TrainerSection.jsx
@@ -3,8 +3,8 @@ const TrainerSection = () => {
     return (
         <div className="py-24 px-5 lg:px-20">
             <div className="border-l-2 border-[#EE6C0C] pl-6">
-                <h4 className="font-medium text-xl">Awsome Process</h4>
-                <h2 className="font-bold uppercase text-[#EE6C0C] text-3xl">Our Process</h2>
+                <h4 className="font-medium text-xl">Our Strongest Team</h4>
+                <h2 className="font-bold uppercase text-[#EE6C0C] text-3xl">Meet Expert Trainers</h2>
             </div>
         </div>
     );
