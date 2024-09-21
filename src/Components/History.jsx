d6fd6e6854284addbb2a8eb8f9d57c440c187e17
abunaim34 <farhanadnanfarabi@gmail.com>
2024-09-21 23:03:02 +0600

    responsive okay

--- src/Components/History.jsx
+++ src/Components/History.jsx
@@ -5,7 +5,7 @@ const History = () => {
     return (
         <div className="relative">
             <div className="absolute bg-opacity-60 bg-[url('/client.jpg')] bg-fixed inset-0"></div>
-            <div className="relative bg-cover h-screen bg-no-repeat grid grid-cols-1 md:grid-cols-4 gap-8">
+            <div className="relative bg-cover md:h-screen h-[846px] bg-no-repeat grid grid-cols-1 md:grid-cols-4 gap-8">
                 <div className="text-center flex flex-col items-center justify-center">
                     <SlCalender className=" text-6xl text-[#EE6C0C]" />
                     <h1 className="text-5xl mt-3 font-bold font-sans">586</h1>
