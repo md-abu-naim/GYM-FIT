bf0d41b00c855d5a4e3b5e990e13e5c35eed4682
abunaim34 <farhanadnanfarabi@gmail.com>
2024-09-21 23:47:12 +0600

    getInTuch section is complate

--- src/Components/GetInTouch.jsx
+++ src/Components/GetInTouch.jsx
@@ -10,14 +10,14 @@ const GetInTouch = () => {
                 <h1 className="text-3xl pb-10 font-bold uppercase">Get In touch</h1>
                 <form action="">
                     <div className="border-b pt-4">
-                        <input placeholder="NAME" type="text" name="name" className=" bg-transparent w-full text-white border-none  border-white" id="" />
+                        <input placeholder="NAME" type="text" name="name" className=" bg-transparent w-full text-white border-none  border-white" />
 
                     </div>
                     <div className="border-b pt-3 mt-4">
-                        <input placeholder="EMAIL" type="email" name="email" className="bg-transparent w-full text-white border-none  border-white" id="" />
+                        <input placeholder="EMAIL" type="email" name="email" className="bg-transparent w-full text-white border-none  border-white" />
                     </div>
                     <div className="border-b pt-3 mt-5">
-                        <input placeholder="MESSAGE" type="text" name="name" className="bg-transparent w-full text-white border-none  border-white" id="" />
+                        <input placeholder="MESSAGE" type="text" name="name" className="bg-transparent w-full text-white border-none  border-white" />
                     </div>
                     <button className="btn rounded-3xl mt-6 px-10 hover:bg-[#252322] bg-[#EE6C0C] border-none text-white uppercase"> send message</button>
                 </form>
