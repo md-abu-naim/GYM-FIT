bbdcadf4f229ccc1c35d5de521d621f047aa4980
abunaim34 <farhanadnanfarabi@gmail.com>
2024-09-22 20:15:08 +0600

    start 404 page

---
+++ src/app/error.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+const error = () => {
+    return (
+        <div className="bg-[url('/bg.jpg')] bg-cover bg-no-repeat h-screen">
+            error
+        </div>
+    );
+};
+
+export default error;
