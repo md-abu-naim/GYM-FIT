98ab1449280276c37b6fd5a1df46f9266e2229ea
abunaim34 <farhanadnanfarabi@gmail.com>
2024-09-22 21:54:07 +0600

    this is page is desgign okay

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -5,15 +5,16 @@ import Link from "next/link";
 
 const Login = () => {
     return (
-        <div className="bg-[url('/bg.jpg')] h-screen text-white bg-cover bg-no-repeat">
+        <div className="bg-[url('/bg.jpg')] relative h-[800px] text-white bg-cover bg-no-repeat">
             <Navber />
             <div className="flex flex-col gap-8 pr-5 lg:pr-20 md:flex-row justify-between items-center">
                 <div className="w-1/2">
-                    <Image src={'/login.webp'} className=" w-[500px]" width={300} height={40} alt="" />
+                    <Image src={'/login.webp'} className="opacity-70 w-[500px]" width={300} height={40} alt="" />
+                    <Image src='/register.png' className="absolute top-0 left-0 bottom-3 w-[680px]" width={400} height={400} alt="" />
                 </div>
-                <div className="lg:w-1/2 w-full py-10">
-                    <h1 className="text-3xl text-[#EE6C0C] pb-10 font-bold uppercase">Enroll to fitness</h1>
-                    <form action="">
+                <div className="lg:w-1/2 text-center w-full py-10">
+                    <h1 className="text-4xl text-[#EE6C0C] pb-10 font-bold uppercase">Continue with your fitness</h1>
+                    <form className="">
                         <div className="border-b pt-4">
                             <input placeholder="NAME" type="text" name="name" className=" bg-transparent w-full text-white border-none  border-white" />
 
@@ -24,12 +25,16 @@ const Login = () => {
                         <div className="border-b pt-3 mt-5">
                             <input placeholder="PASSWORD" type="password" name="password" className="bg-transparent w-full text-white border-none  border-white" />
                         </div>
-                        <button className="btn rounded-3xl mt-6 px-10 hover:bg-[#252322] bg-[#EE6C0C] border-none text-white uppercase"> Enroll now</button>
+                        <button className="btn w-full rounded-3xl mt-6 px-10 hover:bg-[#252322] bg-[#EE6C0C] border-none text-white uppercase"> Enroll now</button>
                     </form>
-                    <Link href='/register'>Dont have an account? Create One </Link>
+                    <div className="flex mb-2 items-center justify-between gap-2">
+                        <button className="btn rounded-3xl mt-6 px-10 hover:bg-[#252322] bg-[#EE6C0C] border-none text-white uppercase w-1/2"> Enroll with Google</button>
+                        <button className="btn rounded-3xl mt-6 px-10 hover:bg-[#252322] bg-[#EE6C0C] border-none text-white uppercase w-1/2"> Enroll with github</button>
+                    </div>
+                    <Link href='/register' className="">Dont have an account? <span className="text-[#EE6C0C] underline">Create One</span> </Link>
                 </div>
             </div>
-            <Footer  />
+            <Footer />
         </div>
     );
 };
